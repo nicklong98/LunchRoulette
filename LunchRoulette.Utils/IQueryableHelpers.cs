59563ce9c100b7d505beb54ed5cafaf768e985be
Nick Long <nick@singularity-id.com>
2018-07-20 19:34:27 -0400

    added IQueryable<T> extensions

---
+++ LunchRoulette.Utils/IQueryableHelpers.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LunchRoulette.Utils.IQueryableHelpers
+{
+
+    public class QueryableExtender<T>
+    {
+        protected IQueryable<T> InvokeOn { get; }
+
+        public QueryableExtender(IQueryable<T> invokeOn)
+        {
+            InvokeOn = invokeOn;
+        }
+
+        public async Task<T> SingleOrThrowAsync<E>() where E : Exception
+        {
+            if (InvokeOn == null) throw new ArgumentException(nameof(InvokeOn));
+            var list = InvokeOn as IList<T>;
+            if (list != null)
+            {
+                switch (list.Count)
+                {
+                    case 0:
+                        throw Activator.CreateInstance<E>();
+                    case 1:
+                        return list[1];
+                }
+            }
+            else
+            {
+                using (var enumerator = InvokeOn.ToAsyncEnumerable().GetEnumerator())
+                {
+                    if (!await enumerator.MoveNext())
+                        throw Activator.CreateInstance<E>();
+                    var current = enumerator.Current;
+                    if (!await enumerator.MoveNext())
+                        return current;
+                }
+            }
+            throw Activator.CreateInstance<E>();
+        }
+
+        public async Task<T> SingleOrThrowAsync<E>(Func<T, bool> predicate) where E : Exception
+        {
+            return await InvokeOn.Where(predicate).AsQueryable().Extend().SingleOrThrowAsync<E>();
+        }
+
+        public T SingleOrThrow<E>() where E : Exception
+        {
+            if (InvokeOn == null) throw new ArgumentNullException(nameof(InvokeOn));
+            IList<T> list = InvokeOn as IList<T>;
+            if (list != null)
+            {
+                switch (list.Count)
+                {
+                    case 0:
+                        throw Activator.CreateInstance<E>();
+                    case 1:
+                        return list[0];
+                }
+            }
+            else
+            {
+                using (IEnumerator<T> enumerator = InvokeOn.GetEnumerator())
+                {
+                    if (!enumerator.MoveNext())
+                        throw Activator.CreateInstance<E>();
+                    T current = enumerator.Current;
+                    if (!enumerator.MoveNext())
+                        return current;
+                }
+            }
+            throw Activator.CreateInstance<E>();
+        }
+    }
+
+    public static class IQueryableExtensions
+    {
+        public static QueryableExtender<T> Extend<T>(this IQueryable<T> source)
+        {
+            return new QueryableExtender<T>(source);
+        }
+    }
+}
