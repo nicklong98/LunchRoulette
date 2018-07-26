36bbe422b5895503c9ea64acbfa841d897a08943
Nick Long <nick.long@rtslabs.com>
2018-07-26 13:54:43 -0400

    some cleanup

--- LunchRoulette.Entities/Entities.cs
+++ LunchRoulette.Entities/Entities.cs
@@ -6,7 +6,6 @@ namespace LunchRoulette.Entities
     public class Cuisine
     {
         public int Id { get; set; }
-        [Required]
         public string Name { get; set; }
 
         public Cuisine() { }
